bef021dfd51ac731b4118cd19279a755f4f1ef98
Vinicius Pasqualini Carvalho <Vinicius Pasqualini Carvalho>
2019-06-20 12:22:59 +0100

    Name refactoring

---
+++ EstablishmentReviewService/src/main/java/com/ubiwhere/repository/EstablishmentReviewRepository.java
@@ -0,0 +1,9 @@
+package com.ubiwhere.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ubiwhere.model.EstablishmentReviewEntity;
+
+public interface EstablishmentReviewRepository extends CrudRepository<EstablishmentReviewEntity,Long>{
+
+}
