764c317671d01816997ef9304ee32b9758f2499f
vinicius <Vinicius Pasqualini Carvalho>
2019-06-22 16:12:24 +0100

    Include swagger for the EstablishmentService

---
+++ EstablishmentService/src/main/java/com/ubiwhere/EstablishmentService/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.ubiwhere.EstablishmentService.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.ubiwhere.EstablishmentService.web"))              
+          .paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiInfo());                                           
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Establishment Agregation API",
+        		"API to agregate information from Establishment Reviews and FHRS API", 
+        		"1.0", 
+        		"urn:tos", 
+        		new Contact("Vinicius Pasqualini Carvalho", " https://www.linkedin.com/in/viniciuspc/en", "pasqualini9@gmail.com"), 
+        		"Apache 2.0", 
+        		"http://www.apache.org/licenses/LICENSE-2.0", 
+        		new ArrayList<VendorExtension>());
+    }
+
+}
--- EstablishmentService/src/main/java/com/ubiwhere/EstablishmentService/web/EstablishmentServiceController.java
+++ EstablishmentService/src/main/java/com/ubiwhere/EstablishmentService/web/EstablishmentServiceController.java
@@ -15,6 +15,8 @@ import com.ubiwhere.EstablishmentService.model.FHRS.Establishment;
 import com.ubiwhere.EstablishmentService.service.EstablishmentFHRSService;
 import com.ubiwhere.EstablishmentService.service.EstablishmentReviewService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class EstablishmentServiceController {
 	
@@ -26,6 +28,7 @@ public class EstablishmentServiceController {
 	
 	
 	@GetMapping(value="estabilishment/{establishment_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ApiOperation(value="Return agregation information for the given establishment", notes=" Will consult the Establishment Review microservice as well as the FHRS and agregate the both information in one sigle return.")
 	public EstablishmentAggregation getEstablishmentAggregation(@PathVariable("establishment_id") long id) throws InterruptedException, ExecutionException {
 		
 		
