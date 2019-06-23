89abd6bd837194ddfa75c7625398086881bb1880
vinicius <Vinicius Pasqualini Carvalho>
2019-06-23 11:27:26 +0100

    Unit test to verify if the context starts. Removed unnecessary class

--- EstablishmentReviewsService/src/main/java/com/ubiwhere/config/SwaggerConfig.java
+++ EstablishmentReviewsService/src/main/java/com/ubiwhere/config/SwaggerConfig.java
@@ -19,12 +19,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {                                    
     @Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
+    	return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
           .apis(RequestHandlerSelectors.basePackage("com.ubiwhere.web"))              
           .paths(PathSelectors.any())                          
           .build()
-          .apiInfo(apiInfo());                                           
+          .apiInfo(apiInfo());                                      
     }
     
     private ApiInfo apiInfo() {
